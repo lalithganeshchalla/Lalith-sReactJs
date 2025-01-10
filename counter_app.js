a8b952800ea584617c1c8c18f9d34d6e7127ee17
lalithganeshchalla <challalalithganesh@gmail.com>
2025-01-11 00:47:01 +0530

    Add files via upload

---
+++ counter_app.js
@@ -0,0 +1,45 @@
+const incrementBtn = document.getElementById('increment');
+const resetBtn = document.getElementById('reset');
+const decrementBtn = document.getElementById('decrement');
+
+let counter = 0;
+
+incrementBtn.addEventListener('click', incrementCounter);
+decrementBtn.addEventListener('click', decrementCounter);
+resetBtn.addEventListener('click', resetCounter);
+
+function incrementCounter() {
+    counter++;
+    updatecounter();
+}
+
+function decrementCounter() {
+    if(counter > 0)   
+    counter--;
+    updatecounter();
+}
+
+function resetCounter() {
+    if(counter==0){
+        alert("Counter is already zero");
+        return;
+    }
+
+
+    counter = 0;
+    updatecounter();
+}
+
+function updatecounter() {
+    const counterValue = document.getElementById('count');
+    counterValue.textContent = counter;
+    if (counter > 10 && counter < 20 ) { 
+        document.body.style.backgroundColor = "green"; 
+    } else if (counter >= 20) { 
+        document.body.style.backgroundColor = "rgb(169, 18, 224)"; 
+        updatecounter(); // Call updatecounter again to update the displayed value 
+        } else { 
+            document.body.style.backgroundColor = "rgb(169, 18, 224)"; 
+        }
+    }
+
